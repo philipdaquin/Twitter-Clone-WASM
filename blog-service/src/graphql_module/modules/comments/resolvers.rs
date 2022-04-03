030488be190eeca5df225f458c4c02f376ab6701
philipdaquin <philipdaquin20@gmail.com>
2022-04-03 14:22:00 +0800

    gql queriees

--- blog-service/src/graphql_module/modules/comments/resolvers.rs
+++ blog-service/src/graphql_module/modules/comments/resolvers.rs
@@ -1,19 +1,36 @@
-use super::providers::*;
+use super::providers::{self, *};
 use async_graphql::*;
 use async_graphql::Error;
 use super::models::*;
-
+use crate::graphql_module::context::get_conn_from_ctx;
 
 #[derive(Default)]
 pub struct CommentQuery;
 
-#[Object]
+// #[Object]
 impl CommentQuery { 
     async fn get_all_comments(&self, ctx: &Context<'_>) -> Result<Vec<CommentObject>, Error> {
-        todo!()
+        let conn = get_conn_from_ctx(ctx);
+        let comments = providers::get_all_comments(&conn)
+            .expect("Could not get Comment")
+            .iter()
+            .map(CommentObject::from)
+            .collect();
+        Ok(comments)
     }
-    async fn get_comment_by_post(&self, ctx: &Context<'_>) -> Result<CommentObject, Error> {
-        todo!()
+    async fn get_comment_by_post(&self, ctx: &Context<'_>, comment_id: ID) -> Result<Vec<CommentObject>, Error> {
+        let conn = get_conn_from_ctx(ctx);
+        let comment_id = comment_id
+            .to_string()
+            .parse::<i32>()
+            .expect("");
+        let comment = providers::get_comments_by_post(comment_id, &conn)
+            .expect("")
+            .iter()
+            .map(CommentObject::from)
+            .collect();
+
+        Ok(comment)
     }
 }
 
