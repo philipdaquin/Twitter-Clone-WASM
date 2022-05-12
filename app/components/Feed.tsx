2deb1e79e4cb3c187ca76b1fd09bcdd533f5f465
philipdaquin <philipdaquin20@gmail.com>
2022-05-12 16:45:07 +0800

    Implemeneted refresh tweets

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RefreshIcon } from '@heroicons/react/outline'
 import TweetBox from './TweetBox'
 import { Tweet } from '../typings'
 import TweetComponent from '../components/Tweet' 
+import { fetchTweets } from '../utils/fetchTweets'
 
 interface Props { 
     tweets: Tweet[]
 }
 
-function Feed({tweets}: Props) {
-  
+function Feed({tweets: tweetsProp}: Props) {
+    const [tweets, setTweets] = useState<Tweet[]>(tweetsProp)
+    console.log(tweets)
+
+    const handleRequest = async () => { 
+        //  Fetch New Recent Tweets 
+        const tweets = await fetchTweets();
+        //  Set the Set to this Newtweets 
+        setTweets(tweets)
+    }
+
     return (
     <div className="col-span-7 lg:log-span-5 border-x">
         <div className="flex items-center justify-between">
             <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
-            <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter 
+            <RefreshIcon onClick={handleRequest} className='h-8 w-8 cursor-pointer text-twitter 
                 mr-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
         </div>
         {/* Tweet bo</div>x */}
--- app/components/Tweet.tsx
+++ app/components/Tweet.tsx
@@ -53,7 +53,7 @@ function Tweet({tweet} : Props) {
                     
                     <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
                         <ChatAlt2Icon className='h-5 w-5'/>
-                        <p></p>
+                        <p>{comments.length}</p>
                     </div>
                     
                     <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
@@ -68,25 +68,33 @@ function Tweet({tweet} : Props) {
                         <UploadIcon className='h-5 w-5'/>
                     </div>
                 </div>
-                
-                {/* Comment Box Login */}
-                {/* {comments?.map(comment => { 
-                    <div key={comment.id}>
-                        <img 
-                            src={comment.profile_img} 
-                            className='h-7 w-7 object-cover ' 
-                            alt="" />
-                        <div>
-                            <div>
-                                <p>{comment.username}</p>
-                                <p>@{comment.username
-                                    .replace(/\s+/g, '').toLowerCase()}
-                                </p>
-                            </div>                            
-                        </div>
-                    </div>
-                })} */}
 
+                            
+          
+                {comments?.length > 0 && (
+                    <div className='my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll 
+                            border-t border-gray-100 p-5'>
+                        {comments?.map(comment => { 
+                            <div key={comment.id} className="flex space-x-2 ">
+                                <hr className='absolute left-5 top-10 h-8 border-x border-twitter/30' />
+                                <img
+                                    src={comment.profile_img}
+                                    className='mt-2 h-7 w-7 object-cover rounded-full'
+                                    alt="" />
+                                <div>
+                                    <div className='flex items-center space-x-1'>
+                                        <p className='mr-1 font-bold'>{comment.username}</p>
+                                        <p className='hidden text-sm text-gray-500 lg:inline '>@{comment.username
+                                            .replace(/\s+/g, '').toLowerCase()}
+                                        </p>
+                                        <TimeAgo className="text-sm text-gray-500" date={comment.created_at} />
+                                    </div>
+                                    <p>{comment.comment}</p>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                )}
             </div>
         </>
     )
