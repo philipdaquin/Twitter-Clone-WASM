ee8fe11cf61b105baf8ead9f75d1f8be22e218d7
philipdaquin <philipdaquin20@gmail.com>
2022-05-12 19:30:03 +0800

    Added authentication and session

---
+++ app/pages/api/auth/[...nextauth].js
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import TwitterProvider from "next-auth/providers/twitter"
+
+
+
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        version: '2.0'
+    }),
+    TwitterProvider({ 
+        clientId: process.env.TWITTER_CLIENT_ID,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET,
+        version: '2.0'
+
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      version: '2.0'
+    }),
+  ],
+})
